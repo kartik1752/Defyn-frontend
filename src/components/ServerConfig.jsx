7e424305fcff53b125d2f984f7228bac1ce53b33
kartik1752 <kartikpasricha3@gmail.com>
2026-03-11 11:14:42 +0530

    y

--- src/components/ServerConfig.jsx
+++ src/components/ServerConfig.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import "./ServerConfig.css";
+import "../pages/ServerConfig.css";
 
 function ServerConfig() {
 
